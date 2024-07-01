f4ce78435817a734c3f83447bb7af9669fe97710
fvalzano <f.valzano@prinsesmaximacentrum.nl>
2024-07-01 17:11:41 +0200

    Data fetching pipeline finalised

--- pipelines/Data_fetching/Data_fetching.sh
+++ pipelines/Data_fetching/Data_fetching.sh
@@ -1,24 +1,37 @@
 #!/bin/bash
-#SBATCH --time=00:20:00
+#SBATCH --time=01:00:00
 #SBATCH --mem=64GB
 #SBATCH --job-name=Data_fetching
 #SBATCH -o log.out
 #SBATCH -e errlog.out
 
 #Run conversion of IDs
-Rscript /hpc/pmc_kool/fvalzano/PostDoc_PMC/pipelines/ID_Conversion.R
+module use --append /hpc/local/Rocky8/pmc_kool/modulefiles
+module load R/4.3.0
+Rscript /hpc/pmc_kool/fvalzano/PostDoc_PMC/pipelines/Data_fetching/ID_Conversion.R
 
 #Set up date and requester
 Date="20240701"
 Requester="Francesco"
 Output_folder="${Date}_${Requester}"
 cd Requests
-mkdir -p "$Output_folder"
-
+mkdir -p "$Output_folder/RNAseq_Patient_Biosource"
+cd ..
 RNAseq_Patient_Biosource_input_file="IDs/RNAseq_Patient_Biosource.txt"
 # Read each label in the input file
 while IFS= read -r label; do
-mkdir "Requests/$Output_folder/$label"
+mkdir "Requests/$Output_folder/RNAseq_Patient_Biosource/$label"
     # Search for files containing the label in their name
-wget -P "Requests/$Output_folder/$label" --user fvalzano --password Brindisi.2021 "https://files.bioinf.prinsesmaximacentrum.nl/shares/PMCLAB2020-142/" --accept="PMLBM000IZI.gene_id.exon.counts.txt"
+wget -m -r -nd -np -P "Requests/$Output_folder/RNAseq_Patient_Biosource/$label" --user fvalzano --password Brindisi.2021 -e robots=off "https://files.bioinf.prinsesmaximacentrum.nl/shares/PMCLAB2020-142/" --accept=${label}*gene_id.exon.counts.txt
 done < "$RNAseq_Patient_Biosource_input_file"
+
+cd Requests
+mkdir -p "$Output_folder/RNAseq_Tumoroid_Biomaterial"
+cd ..
+RNAseq_Tumoroid_Biomaterial_input_file="IDs/RNAseq_Tumoroid_Biomaterial.txt"
+# Read each label in the input file
+while IFS= read -r label; do
+mkdir "Requests/$Output_folder/RNAseq_Tumoroid_Biomaterial/$label"
+    # Search for files containing the label in their name
+wget -m -r -nd -np -P "Requests/$Output_folder/RNAseq_Tumoroid_Biomaterial/$label" --user fvalzano --password Brindisi.2021 -e robots=off "https://files.bioinf.prinsesmaximacentrum.nl/shares/PMCLAB2020-142/" --accept=${label}*gene_id.exon.counts.txt
+done < "$RNAseq_Tumoroid_Biomaterial_input_file"
