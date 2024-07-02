32b6d3d3e299c6a98d7024c2cc9a9de4acc21098
fvalzano <f.valzano@prinsesmaximacentrum.nl>
2024-07-02 13:03:38 +0200

    DESeq2 analysis - added QC, DEG and Normalization

--- pipelines/DESeq2/DESeq2.R
+++ pipelines/DESeq2/DESeq2.R
@@ -1,5 +1,9 @@
 library(DESeq2)
 library(purrr)
+library(apeglm)
+library(ggplot2)
+library(umap)
+library(uwot)
 #Load bulk RNAseq runs from specific requests folder
 ##Modify request folder
 result_directory = "20240701_Francesco"
@@ -15,6 +19,41 @@ for (file in RNAseq_files) {
 }
 
 ##Merge the single runs in one
+RNAseq_merged = RNAseq_runs[[1]]
+for (i in 2:length(RNAseq_runs)) {
+  RNAseq_merged <- cbind(RNAseq_merged, RNAseq_runs[[i]])
+}
 
-RNAseq_merge = reduce(RNAseq_runs, function(x, y) merge(x, y, by = "Gene_name"))
+##Remove Gene_name columns duplicates
+RNAseq_merged = as.data.frame(RNAseq_merged, row.names = RNAseq_merged$Gene_name)
+RNAseq_merged = RNAseq_merged[, -grep("Gene_name", names(RNAseq_merged))]
+##Rename dataframe with IDs and material of origin
+IDs_patients = paste0(IDs, "_Patients")
+IDs_tumoroid = paste0(IDs, "_Tumoroid")
+colnames(RNAseq_merged) = c(IDs_patients, IDs_tumoroid)
+##Write analysis design and set up dds object
+design = as.data.frame(rep(c("Patient", "Tumoroid"), each = 3))
+colnames(design) = "material"
+design$IDs = rep(IDs,2)
+rownames(design) = colnames(RNAseq_merged)
+dds <- DESeqDataSetFromMatrix(countData = RNAseq_merged,
+                              colData = design,
+                              design = ~ material)
 
+##QC the dds object
+smallestGroupSize <- 3
+retain <- rowSums(counts(dds) >= 10) >= smallestGroupSize
+dds <- dds[retain,]
+
+##Differentially Expressed Gene Analysis with apeglm shrinkage of fold changes
+resultsNames(dds)
+dds <- DESeq(dds)
+res <- results(dds)
+resLFC <- lfcShrink(dds, coef="material_Tumoroid_vs_Patient", type="apeglm")
+DEG = as.data.frame(resLFC)
+DEG = DEG[DEG$padj<=0.05,]
+
+
+#Vst normalization and dimensionality reduction analysis
+vsd <- vst(dds, blind=FALSE)
+plotPCA(vsd, intgroup=c("material")) + stat_ellipse()
