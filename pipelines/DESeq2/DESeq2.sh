3a59b6ef17c57315a1337ac45df87b2eec53375c
fvalzano <f.valzano@prinsesmaximacentrum.nl>
2024-07-02 17:00:38 +0200

    Added job submission script

---
+++ pipelines/DESeq2/DESeq2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --time=00:10:00
+#SBATCH --mem=64GB
+#SBATCH --job-name=DESeq2
+#SBATCH -o log.out
+#SBATCH -e errlog.out
+
+#Set up date and requester
+Date="20240701"
+Requester="Francesco"
+Output_folder="${Date}_${Requester}"
+cd Requests
+mkdir -p "$Output_folder"
+cd ..
+
+#Run DESeq2
+module use --append /hpc/local/Rocky8/pmc_kool/modulefiles
+module load R/4.3.0
+Rscript /hpc/pmc_kool/fvalzano/PostDoc_PMC/pipelines/DESeq2/DESeq2.R
