db675d4d087f84694ec22a01c4db78fba429046c
fvalzano <f.valzano@prinsesmaximacentrum.nl>
2024-07-03 17:40:02 +0200

    Changed directories of reference genome

--- pipelines/remapping/Re-alignment.sh
+++ pipelines/remapping/Re-alignment.sh
@@ -1,4 +1,4 @@
 module use --append /hpc/local/Rocky8/pmc_research/etc/modulefiles
 #load cellranger version used for previous runs
 module load cellranger/6.1.1
-cellranger count --localcores=6 --localmem=32 --localvmem=96 --include-introns --transcriptome=/hpc/pmc_kool/fvalzano/PostDoc_PMC/pipelines/remapping/refdata-gex-GRCh38-2020-A --fastqs=/hpc/pmc_kool/fvalzano/PostDoc_PMC/remapping/data_to_remap/YourFastqFiles --sample=YourSample --id YourID
+cellranger count --localcores=6 --localmem=32 --localvmem=96 --include-introns --transcriptome=/hpc/pmc_kool/fvalzano/Reference_genomes/refdata-gex-GRCh38-2020-A --fastqs=/hpc/pmc_kool/fvalzano/PostDoc_PMC/remapping/data_to_remap/YourFastqFiles --sample=YourSample --id YourID
