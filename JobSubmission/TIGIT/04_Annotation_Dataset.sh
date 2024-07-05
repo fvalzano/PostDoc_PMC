3bab17477060cdd2c57425cb65028df3becb528d
fvalzano <f.valzano@prinsesmaximacentrum.nl>
2024-07-05 13:34:44 +0200

    Initial commit_added job submission folder and log out files

---
+++ JobSubmission/TIGIT/04_Annotation_Dataset.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --time=48:00:00
+#SBATCH --mem=256GB
+#SBATCH --job-name=Annotation_SingleR
+#SBATCH -o log.out
+#SBATCH -e errlog.out
+#SBATCH --mail-user=f.valzano@prinsesmaximacentrum.nl 
+
+module use --append /hpc/local/Rocky8/pmc_kool/modulefiles
+module load R
+Rscript /hpc/pmc_kool/fvalzano/PostDoc_PMC/R/TME/Scripts_TIGIT/04_Annotation_Dataset.R
