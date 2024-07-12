e4ca0bdd9684785f7c28c55aef6b40be7117804e
fvalzano <f.valzano@prinsesmaximacentrum.nl>
2024-07-12 09:36:33 +0200

    Optimization memory consumption

--- R/TME/Scripts_FullDataset/02_TME_InferCNV.R
+++ R/TME/Scripts_FullDataset/02_TME_InferCNV.R
@@ -17,7 +17,7 @@ infercnv = CreateInfercnvObject(raw_counts_matrix=counts_matrix,
                                     delim="\t",
                                     gene_order_file="/hpc/pmc_kool/fvalzano/Rstudio_Test1/TME/TME_files_March24/InferCNV/hg38_gencode_v27.txt",
                                     ref_group_names=c("0", "28", "41", "35"))
-
+rm(counts_matrix, scrna)
 infercnv = infercnv::run(infercnv,
                               cutoff=0.1,
                              out_dir="/hpc/pmc_kool/fvalzano/Rstudio_Test1/TME/TME_files_March24/InferCNV/Output/WithAnnotation", 
@@ -25,13 +25,14 @@ infercnv = infercnv::run(infercnv,
                              denoise=TRUE,
                              HMM=TRUE,
                              resume_mode = T)
-
+scrna = read_rds("/hpc/pmc_kool/fvalzano/Rstudio_Test1/TME/TME_files_March24/scrna_harmony.rds")
 scrna = infercnv::add_to_seurat(seurat_obj = scrna,
                                            infercnv_output_path = "/hpc/pmc_kool/fvalzano/Rstudio_Test1/TME/TME_files_March24/InferCNV/Output/WithAnnotation",
                                            top_n = 15)
 write_rds(scrna, "/hpc/pmc_kool/fvalzano/Rstudio_Test1/TME/TME_files_March24/Seurat_subsets/Post_InferCNV/scrna_infercnv_with_annotations.rds")
 
 #Without Annotation
+scrna = read_rds("/hpc/pmc_kool/fvalzano/Rstudio_Test1/TME/TME_files_March24/scrna_harmony.rds")
 scrna$AllCells = "Cells"
 Idents(scrna) = "AllCells"
 counts_matrix = GetAssayData(scrna, layer="counts")
@@ -43,7 +44,7 @@ infercnv = CreateInfercnvObject(raw_counts_matrix=counts_matrix,
                                     delim="\t",
                                     gene_order_file="/hpc/pmc_kool/fvalzano/Rstudio_Test1/TME/TME_files_March24/InferCNV/hg38_gencode_v27.txt",
                                     ref_group_names=c())
-
+rm(counts_matrix, scrna)
 infercnv = infercnv::run(infercnv,
                               cutoff=0.1,
                              out_dir="/hpc/pmc_kool/fvalzano/Rstudio_Test1/TME/TME_files_March24/InferCNV/Output/NoAnnotation", 
@@ -52,7 +53,7 @@ infercnv = infercnv::run(infercnv,
                              HMM=TRUE,
                              resume_mode = T,
                              k_obs_groups = 2)
-
+scrna = read_rds("/hpc/pmc_kool/fvalzano/Rstudio_Test1/TME/TME_files_March24/scrna_harmony.rds")
 scrna = infercnv::add_to_seurat(seurat_obj = scrna,
                                            infercnv_output_path = "/hpc/pmc_kool/fvalzano/Rstudio_Test1/TME/TME_files_March24/InferCNV/Output/NoAnnotation",
                                            top_n = 15)
