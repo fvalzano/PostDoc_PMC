bd577285f1d80bcf2012f5e5759fb19585b3af70
fvalzano <f.valzano@prinsesmaximacentrum.nl>
2024-08-05 16:52:40 +0200

    Adjusted destination directory of barcode files

--- pipelines/wdl_scripts/bulkRNA_quantification/Barcodes_search.sh
+++ pipelines/wdl_scripts/bulkRNA_quantification/Barcodes_search.sh
@@ -11,12 +11,11 @@ for dir in */; do
         for subdir in */; do
             cd "$subdir"
             sample=$(for name in `ls`; do echo $name | cut -d - -f 1-2; done | uniq)  #this extracts the sample identifier
-            
-            
-            #Move the barcode file to the input folder - PICKUP FROM HERE
-            zcat $(realpath $(ls | grep _R1.fastq.gz)) | head -1 | rev | cut -d ":" -f 1 | rev > barcodes_${sample}.txt
-            scp barcodes_${sample}.txt /hpc/pmc_kool/fvalzano/wdl_pipeline_v12.1.0/wdl/inputs/barcodes/
-            rm barcodes_${sample}.txt
+            #Move the barcode file to the input folder 
+            zcat $(realpath $(ls | grep _R1.fastq.gz)) | head -1 | rev | cut -d ":" -f 1 | rev > barcodes_${sample}
+            mkdir -p "/hpc/pmc_kool/fvalzano/wdl_pipeline_v12.1.0/wdl/inputs/barcodes/${dir}${subdir}"
+            scp barcodes_${sample} "/hpc/pmc_kool/fvalzano/wdl_pipeline_v12.1.0/wdl/inputs/barcodes/${dir}${subdir}"
+            rm barcodes_${sample}
             # Return to the Fastq directory
             cd ..
         done
