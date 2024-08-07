041e628aee1b47c46c96ff57e4ee9dc82fce36ab
fvalzano <f.valzano@prinsesmaximacentrum.nl>
2024-08-07 09:38:28 +0200

    Pipeline for ubam to count tables adapted for working for multiple samples, for storage purposes, the loop works one by one

---
+++ pipelines/wdl_scripts/bulkRNA_quantification/ITCCP4/run_rna_fusion_workflow_fv_looped.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+directories=()
+cd /hpc/pmc_kool/fvalzano/wdl_pipeline_v12.1.0/wdl/inputs/ubam2counts/ubam2counts_ITCCP4
+for dir in */; do
+    cd $dir
+        for sample in `ls`; do
+        cd $sample
+        directories+=("$(pwd)")
+        cd ..
+        done 
+    cd ..
+done
+
+cd /hpc/pmc_kool/fvalzano/wdl_pipeline_v12.1.0/cromwell
+workflow_source='rna_fusions_germline_snv_workflow.wdl'
+workflow_dependencies="*.wdl"
+
+set -o errexit
+set -o pipefail
+
+source ./utils/get_cromwell_credentials.sh
+source ./utils/starting_cromwell_jobs.sh
+
+cd ../wdl
+rm -f modules.zip
+zip -q modules.zip $workflow_dependencies
+
+for dir in "${directories[@]}"; do
+    workflow_inputs=${dir}/rna_fusions_germline_snv_inputs_no_molgenis_fv.json
+    echo "Submitting workflow for ${workflow_inputs}"
+    _caas_submit \
+        "$workflow_source" \
+        "$workflow_inputs" \
+        "modules.zip"
+    sleep 43200 
+done
