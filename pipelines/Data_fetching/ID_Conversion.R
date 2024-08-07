0b28ecdba99cf1f1e3e0605186313c888ffe4731
fvalzano <f.valzano@prinsesmaximacentrum.nl>
2024-08-07 10:18:24 +0200

    Changed directory to database instead of script folder

--- pipelines/Data_fetching/ID_Conversion.R
+++ pipelines/Data_fetching/ID_Conversion.R
@@ -2,7 +2,7 @@ library(readxl)
 library(dplyr)
 library(stringr)
 #Load Overview files containing different IDs
-Overview = read_xlsx("/hpc/pmc_kool/fvalzano/Overview/General_overview_patient_seqdata.xlsx")
+Overview = read_xlsx("/hpc/pmc_kool/fvalzano/Overview/General_overview_patient_seqdata_ForHPC.xlsx")
 #Delete potential space in the PMCID - this creates troubles during the fetching procedure
 Overview=Overview %>% 
   mutate(across(where(is.character), str_trim))
@@ -10,7 +10,11 @@ Overview=Overview %>%
 IDs = c()
 IDs = IDs[order(IDs, decreasing = F)]
 #Fetch the different IDs for sample type
-IDs_RNA_Patient_Biosource= Overview[Overview$PMCID %in% IDs,]$'RNAseq Patient Biomaterial'
+IDs_RNA_Patient_Biomaterial= Overview[Overview$PMCID %in% IDs,]$'RNAseq Patient Biomaterial'
 IDs_RNA_Tumoroid_Biomaterial= Overview[Overview$PMCID %in% IDs,]$'RNAseq Tumoroid Biomaterial'
-writeLines(IDs_RNA_Patient_Biosource, "/hpc/pmc_kool/fvalzano/PostDoc_PMC/pipelines/Data_fetching/IDs/RNAseq_Patient_Biomaterial.txt")
-writeLines(IDs_RNA_Tumoroid_Biomaterial, "/hpc/pmc_kool/fvalzano/PostDoc_PMC/pipelines/Data_fetching/IDs/RNAseq_Tumoroid_Biomaterial.txt")
+writeLines(IDs_RNA_Patient_Biomaterial, "/hpc/pmc_kool/fvalzano/pipelines_fv_output/Data_fetching/IDs/RNAseq_Patient_Biomaterial.txt")
+writeLines(IDs_RNA_Tumoroid_Biomaterial, "/hpc/pmc_kool/fvalzano/pipelines_fv_output/Data_fetching/IDs/RNAseq_Tumoroid_Biomaterial.txt")
+#If there are relapse data, run:
+IDs_RNA_Relapse_Biomaterial= Overview[Overview$PMCID %in% IDs,]$'RNAseq Patient Relapse Biomaterial'
+writeLines(IDs_RNA_Relapse_Biomaterial, "/hpc/pmc_kool/fvalzano/pipelines_fv_output/Data_fetching/IDs/RNAseq_Relapse_Biomaterial.txt")
+
