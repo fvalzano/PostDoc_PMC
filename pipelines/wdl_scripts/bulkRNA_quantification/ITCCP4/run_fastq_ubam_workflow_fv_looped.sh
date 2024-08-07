c6bb9552088561bc127bb4032ddc41bf6a897319
fvalzano <f.valzano@prinsesmaximacentrum.nl>
2024-08-07 09:37:40 +0200

    Pipeline for fastq to ubam adapted for working for multiple samples

---
+++ pipelines/wdl_scripts/bulkRNA_quantification/ITCCP4/run_fastq_ubam_workflow_fv_looped.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+##This belongs to the orginal script - flag it and explicit directories 
+#echo "${inputs_json}"
+#
+#if [ "${inputs_json}" == "" ]; then
+#        echo "ERROR:No inputs.json provided\n"
+#        echo "inputs_json=<INPUTS.JSON> $0"
+#        exit 1
+#fi
+
+#This outputs the directory names to feed in the subsequent loop of submission to the CaaS
+directories=()
+cd /hpc/pmc_kool/fvalzano/wdl_pipeline_v12.1.0/wdl/inputs/fastq2ubam/fastq2ubams_ITCCP4
+for dir in */; do
+    cd $dir
+        for sample in `ls`; do
+        cd $sample
+        directories+=("$(pwd)")
+        cd ..
+        done 
+    cd ..
+done
+
+cd /hpc/pmc_kool/fvalzano/wdl_pipeline_v12.1.0/cromwell
+workflow_source='/hpc/pmc_kool/fvalzano/wdl_pipeline_v12.1.0/wdl/multi_fastq_ubam_workflow.wdl'
+workflow_dependencies="*.wdl"
+
+set -o errexit
+set -o pipefail
+
+source ./utils/get_cromwell_credentials.sh
+source ./utils/starting_cromwell_jobs.sh
+
+cd ../wdl
+rm -f modules.zip
+zip -q modules.zip $workflow_dependencies
+
+for dir in "${directories[@]}"; do
+    workflow_inputs=${dir}/fastq_ubam_workflow_fv.inputs.json
+    _caas_submit \
+        "$workflow_source" \
+        "$workflow_inputs" \
+        "modules.zip"
+done
