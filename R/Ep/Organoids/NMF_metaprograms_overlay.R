3045cef0d5f69db43d0b4bf6e7d4d7827eef2199
fvalzano <f.valzano@prinsesmaximacentrum.nl>
2024-09-16 10:26:10 +0200

    Added export rds seurat object with scores

--- R/Ep/Organoids/NMF_metaprograms_overlay.R
+++ R/Ep/Organoids/NMF_metaprograms_overlay.R
@@ -2,18 +2,19 @@ library(Seurat)
 library(readxl)
 library(ggplot2)
 library(circlize)
+library(readr)
 #Load the healthy organoid dataset
 scrna = readRDS("/hpc/pmc_kool/fvalzano/Jupyter/scvelo/Ependymoma/rds files/LX_healthy_final.rds")
 
 #Load the NMF metaprogram from Filbin et al
-NMF_meta = read_xlsx("/hpc/pmc_kool/fvalzano/Rstudio_Test1/Ependymoma_organoids/NMFmetaprorams_from_Filbin/media-5-2.xlsx")
+NMF_meta = read_xlsx("/hpc/pmc_kool/fvalzano/Rstudio_Test1/Ependymoma_organoids/Annotations/NMFmetaprorams_from_Filbin/media-5-2.xlsx")
 colnames(NMF_meta) = NMF_meta[1,]
 NMF_meta[1,] = NA
 NMF_meta = na.omit(NMF_meta)
 for(i in colnames(NMF_meta)){
     scrna = AddModuleScore(scrna, features = list(NMF_meta[[i]]), name = paste0(i, "_score"))
 }
-pdf("/hpc/pmc_kool/fvalzano/Rstudio_Test1/Ependymoma_organoids/Plots/Overlay_meta_unbiased_clustering.pdf", width = 10, height = 7.5)
+pdf("/hpc/pmc_kool/fvalzano/Rstudio_Test1/Ependymoma_organoids/Plots/Overlay_Filbin_unbiased_clustering.pdf", width = 10, height = 7.5)
 DimPlot(scrna,
     group.by = "RNA_snn_res.0.5", 
     label = T) +
@@ -29,7 +30,7 @@ DotPlot(scrna, features = c("Neuroepithelial-like_score1",
                group.by = "RNA_snn_res.0.5")+
         theme(axis.text.x = element_text(angle = 45, hjust = 1))
 dev.off()
-pdf("/hpc/pmc_kool/fvalzano/Rstudio_Test1/Ependymoma_organoids/Plots/Overlay_meta_ownmapping.pdf", width = 10, height = 7.5)
+pdf("/hpc/pmc_kool/fvalzano/Rstudio_Test1/Ependymoma_organoids/Plots/Overlay_Filbin_ownmapping.pdf", width = 10, height = 7.5)
 DimPlot(scrna,
     group.by = "own.mapping", 
     label = T) +
@@ -87,3 +88,5 @@ chordDiagram(chord,
                           "Late RG" = "#00BFC4",
                           "Early RG" = "#C77CFF"))
 dev.off()
+
+write_rds(scrna, "/hpc/pmc_kool/fvalzano/Jupyter/scvelo/Ependymoma/rds files/LX_healthy_final.rds")
